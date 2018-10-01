0377bf8175bd6feca04364dfe130abf9492ac452
stevenzhou3 <stevenzhou3@mc6045-03.labs.sce.carleton.ca>
2018-10-01 12:31:51 -0400

    fixed errors

--- SYSC3600/src/lab4sysc3010/Hello.java
+++ SYSC3600/src/lab4sysc3010/Hello.java
@@ -2,7 +2,7 @@ package lab4sysc3010;
 
 public class Hello {
 
-	public static main(String[] args) 
+	public static void main(String[] args) 
 	{
 		Hello hello = new Hello();
 		hello.methodA();
@@ -10,9 +10,9 @@ public class Hello {
 		hello.methodC();
 	}
 
-	public void methodA(void ) { }
+	public void methodA() { }
 
-	public void methodb(void) { }
+	public void methodB() { }
 
-	public void methodC(void) { }
+	public void methodC() { }
 }
